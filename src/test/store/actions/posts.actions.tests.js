ed583e8fb8e1f4a1b08434ece172e2715d7ff39c
Kavita Nambissan Ganguli <kavitanambissan@gmail.com>
2018-10-31 11:05:30 +0530

    Tests added

---
+++ src/test/store/actions/posts.actions.tests.js
@@ -0,0 +1,32 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import * as postActions from  '../../store/actions/posts.actions';
+
+const middlewares = [thunk];
+
+const mockStore = configureMockStore(middlewares);
+
+describe('Post Actions', () => {
+
+  beforeEach( () => {
+    mockStore.clearActions();
+  })
+
+  describe('Get posts', () => {
+
+    test('Dispatches action and payload', () => {
+      const expectedActions = [
+        {
+          type: "GET_ALL_POSTS", 
+          posts: 1
+        }
+      ];
+
+      mockStore.dispatch(postActions.loadAllPosts());
+      expect(mockStore.getActions()).toEqual(expectedActions);
+    })
+
+  });
+
+});
