f121d09e58d631a2487c49d72d7451058632eb47
Kavita Nambissan Ganguli <kavitanambissan@gmail.com>
2018-10-31 11:11:40 +0530

    Moved test to code folder

--- src/store/actions/posts.actions.test.js
+++ src/store/actions/posts.actions.test.js
@@ -1,7 +1,7 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import * as postActions from  '../../store/actions/posts.actions';
+import * as postActions from  './posts.actions';
 
 const middlewares = [thunk];
 
